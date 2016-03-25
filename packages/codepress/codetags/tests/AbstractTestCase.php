ea05fc282fe11e6eb7728b8fdae0b355374c62df
Eduardo Vieira <eduardovra@gmail.com>
2016-03-25 14:02:13 -0300

    Corrige indentação de tabs para 4 espaços

--- packages/codepress/codetags/tests/AbstractTestCase.php
+++ packages/codepress/codetags/tests/AbstractTestCase.php
@@ -6,20 +6,20 @@ use Orchestra\Testbench\TestCase;
 
 abstract class AbstractTestCase extends TestCase
 {
-	public function migrate()
-	{
-		$this->artisan('migrate', [
-			'--realpath' => realpath(__DIR__ . '/../src/resources/migrations')
-		]);
-	}
+    public function migrate()
+    {
+        $this->artisan('migrate', [
+            '--realpath' => realpath(__DIR__ . '/../src/resources/migrations')
+        ]);
+    }
 
     protected function getEnvironmentSetUp($app)
     {
         $app['config']->set('database.default', 'testbench');
         $app['config']->set('database.connections.testbench', [
-        	'driver' => 'sqlite',
-        	'database' => ':memory:',
-        	'prefix' => ''
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => ''
         ]);
         
     }
--- packages/codepress/codetags/tests/CodeTag/Models/TagTest.php
+++ packages/codepress/codetags/tests/CodeTag/Models/TagTest.php
@@ -7,31 +7,31 @@ use CodePress\CodeTag\Tests\AbstractTestCase;
 
 class TagTest extends AbstractTestCase
 {
-	public function setUp()
-	{
-		parent::setUp();
-		$this->migrate();
-	}
+    public function setUp()
+    {
+        parent::setUp();
+        $this->migrate();
+    }
 
-	public function test_check_tag_name_is_saved()
-	{
-		$newtag = Tag::create(['name' => 'My Tag']);
+    public function test_check_tag_name_is_saved()
+    {
+        $newtag = Tag::create(['name' => 'My Tag']);
 
-		$this->assertEquals('My Tag', $newtag->name);
+        $this->assertEquals('My Tag', $newtag->name);
 
-		$tag = Tag::all()->first();
+        $tag = Tag::all()->first();
 
-		$this->assertEquals('My Tag', $tag->name);
-	}
+        $this->assertEquals('My Tag', $tag->name);
+    }
 
-	public function test_check_that_tags_are_taggable()
-	{
-		$first_tag = Tag::create(['name' => 'First Tag']);
+    public function test_check_that_tags_are_taggable()
+    {
+        $first_tag = Tag::create(['name' => 'First Tag']);
 
-		$second_tag = Tag::create(['name' => 'Second Tag']);
+        $second_tag = Tag::create(['name' => 'Second Tag']);
 
-		$first_tag->taggable()->associate($second_tag);
+        $first_tag->taggable()->associate($second_tag);
 
-		$this->assertEquals($second_tag, $first_tag->taggable);
-	}
+        $this->assertEquals($second_tag, $first_tag->taggable);
+    }
 }
